1dca40ad74b5c8efdeb5afe3cd8a6d4694de99e1
Diego Aliaga <diego.aliaga@helsinki.fi>
2019-02-08 13:35:47 +0200

    added util

---
+++ util/create_new_branch.bash
@@ -0,0 +1,13 @@
+#! /bin/bash
+new_branch_name=v07
+git_path=/homeappl/home/aliagadi/saltena_2018/tucu-april/
+data_link=tucu-april-data
+base_data_path=/wrk/aliagadi/DONOTREMOVE/saltena_2018/
+data_path=${base_data_path}${data_link}-${new_branch_name}
+echo ${data_path}
+
+cd ${git_path}
+mkdir ${data_path}
+rm -f ${data_link}
+ln -s ${data_path} ${data_link}
+
