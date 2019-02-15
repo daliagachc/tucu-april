7104249d3d3a67cf6638ea614ea43e02179a9c90
Diego Aliaga <diego.aliaga@helsinki.fi>
2019-02-15 02:33:48 +0200

    flex

---
+++ flexpart-examples/run_flex.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -e ./run_logs/error%j.txt
+#SBATCH -o ./run_logs/output%j.txt 
+#SBATCH -n 1	
+#SBATCH -t 03:10:00
+#SBATCH -p serial
+#SBATCH --mem-per-cpu=8000
+#SBATCH --mail-type=END
+#SBATCH --mail-user=diego.aliaga@helsinki.fi
+
+# first set the environemt
+source ./env_WRFv4.bash
+
+#run_dir=./tucu-april-data/WPS
+exe=./flexwrf33_gnu_omp
+
+#rm ${run_dir}/Vtable
+#rm ${run_dir}/namelist.wps
+
+#cp ./namelist.wps ${run_dir}/
+#cd ${run_dir}
+
+## run the executable
+srun ${exe} 
+
